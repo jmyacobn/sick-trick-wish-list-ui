595b4477ee9342dfbeccdc73c3f447bfe71cff6b
Jenn Yacoubian <jyacoubian@gmail.com>
2023-01-02 16:15:08 -0700

    Add form

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 import Tricks from '../Tricks/Tricks'
+import Form from '../Form/Form';
 import './App.css';
 
 class App extends Component {
@@ -16,11 +17,16 @@ class App extends Component {
       .then(data => this.setState({tricks: data}))
   }
 
+  addTrick = (newTrick) => {
+    this.setState({ tricks: [...this.state.tricks, newTrick]})
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Sick Trick Wish List</h1>
         <div>
+          <Form addTrick={this.addTrick} />
           <Tricks tricks={this.state.tricks} />
         </div>
       </div>
